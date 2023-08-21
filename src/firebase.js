139db94a4d158aa9bf179d838aa85d54d7a31311
ahaduzzaman5503 <ahaduzzaman5503@gmail.com>
2023-08-22 01:16:21 +0600

    setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0aSKkk_TFSZXTkiy71-JqLRvkH0lvALE",
+  authDomain: "chat-app-bbc66.firebaseapp.com",
+  projectId: "chat-app-bbc66",
+  storageBucket: "chat-app-bbc66.appspot.com",
+  messagingSenderId: "635230774763",
+  appId: "1:635230774763:web:8b2511906aad658f1a65ae"
+};
+
+const app = initializeApp(firebaseConfig);
